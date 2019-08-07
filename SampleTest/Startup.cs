9cfdafa188ba1bdd3b84c0cd05c72304baa7b543
unknown <samuel.konakalla@gmail.com>
2019-08-07 11:30:05 +0000

    all files are moving

---
+++ SampleTest/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SampleTest.Startup))]
+namespace SampleTest
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
